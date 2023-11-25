7e60c5acc8959810cd9e97b6503fd3d0f9ac8768
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-26 01:36:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let hex = '#'
+	for(let i=1;i<4;i++)
+	{
+		hex+=shortHex.charAt(i);
+		hex+=shortHex.charAt(i);
+	}
+	return hex;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
